c398f7bfdf97e3be44d5186527beda102e506a59
denigunawan <artdengun@gmail.com>
2020-09-15 15:19:27 +0700

    first commit

---
+++ src/main/java/com/denigunawan/Belajarapachekafka/BelajarApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.denigunawan.Belajarapachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarApacheKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denigunawan/Belajarapachekafka/BelajarApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denigunawan.Belajarapachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
